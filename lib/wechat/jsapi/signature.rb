da50ca6558fd464f65b3deac186efdd1d8cacf0c
Topbit Du <topbit.du@gmail.com>
2016-09-01 12:35:33 +0800

    1, Comment the calling to the assert_present! method.

--- lib/wechat/jsapi/signature.rb
+++ lib/wechat/jsapi/signature.rb
@@ -4,7 +4,7 @@ class Wechat::JSAPI::Signature
   # http://mp.weixin.qq.com/wiki/11/74ad127cc054f6b80759c40f77ec03db.html#.E9.99.84.E5.BD.951-JS-SDK.E4.BD.BF.E7.94.A8.E6.9D.83.E9.99.90.E7.AD.BE.E5.90.8D.E7.AE.97.E6.B3.95
   def self.create(link, ticket, timestamp, nonce)
 
-    assert_present! :link, link
+    #assert_present! :link, link
     raise ArgumentError.new('The link argument is required.')       if link.blank?
     raise ArgumentError.new('The ticket parameter is required.')    if ticket.blank?
     raise ArgumentError.new('The timestamp parameter is required.') if timestamp.blank?
