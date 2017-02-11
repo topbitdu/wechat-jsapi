e27963927cd16a41f7dc47a024eee23e3f9aa647
Topbit Du <topbit.du@gmail.com>
2017-02-11 09:31:14 +0800

    1, Improve the Ticket class for the document.

--- lib/wechat/jsapi/ticket.rb
+++ lib/wechat/jsapi/ticket.rb
@@ -2,6 +2,7 @@ class Wechat::JSAPI::Ticket
 
   extend Wechat::Core::Common
 
+  ##
   # 获得jsapi_ticket
   # http://mp.weixin.qq.com/wiki/11/74ad127cc054f6b80759c40f77ec03db.html#.E9.99.84.E5.BD.951-JS-SDK.E4.BD.BF.E7.94.A8.E6.9D.83.E9.99.90.E7.AD.BE.E5.90.8D.E7.AE.97.E6.B3.95
   #
